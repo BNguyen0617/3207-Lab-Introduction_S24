e2834d28a970b5a833f3eb232e01e97cc7debe0d
Brian Nguyen <tus01019@temple.edu>
2024-01-19 09:42:05 -0500

    made a new file called random.c that has the randchar in it to generate a random character

--- print_random.c
+++ print_random.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
+#include "random.c"
 
 
 int main()
@@ -12,7 +12,7 @@ int main()
 	srand( (unsigned)time(NULL) );
 
 	printf("Today's random word: ");
-	for(a=0;a<7;a++)
+	for(a=0;a<7;a++)//the for loop will run 7 times and the rand function will print a random character making a  random word
 		putchar( randchar() );
 	putchar('\n');
 
---
+++ random.c
@@ -0,0 +1,6 @@
+//
+// Created by Brian Nguyen on 1/17/24.
+//
+char randchar() {
+    return (rand() % 26) + 'A';// this will return a random letter a through z
+}
